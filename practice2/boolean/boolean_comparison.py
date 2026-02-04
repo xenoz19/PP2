5e35af07e1b708b17bf6b26a6c74a7f571cbeff3
xenoz19 <edgenovnurkanat@gmail.com>
2026-02-04 21:05:37 +0500

    Add boolean comparison example

---
+++ practice2/boolean/boolean_comparison.py
@@ -0,0 +1,11 @@
+# Boolean values as comparison results
+
+a = 7
+b = 3
+
+print(a > b)    # True
+print(a < b)    # False
+print(a == b)   # False
+print(a != b)   # True
+print(a >= b)   # True
+print(a <= b)   # False
